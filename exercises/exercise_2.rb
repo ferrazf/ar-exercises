cbf944dd0482632321fc8ae81e9ad2cd9623aa85
ferrazf <ferrazf9@gmail.com>
2019-05-14 17:35:32 +0000

    Finish exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1  = Store.first;
+@store2Arr = Store.limit(2)
+@store2 = @store2Arr[2]
+@store1.name = "bubbles";
+@store1.save;
