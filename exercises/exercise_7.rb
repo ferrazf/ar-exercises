2dac580aae06ab12cd346f0f5ee3e18dcb112a54
ferrazf <ferrazf9@gmail.com>
2019-05-15 02:53:28 +0000

    Finish exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a new store name: "
+store = gets.chomp
+
+newStore = Store.create(name: store)
+puts newStore.errors.full_messages if newStore.errors
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,44 @@
 class Store < ActiveRecord::Base
     has_many :employees
+
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0
+    }
+
+  validate :has_apparel
+  before_destroy :check_employees
+
+  def has_apparel
+    if !mens_apparel.present? && !womens_apparel.present?
+      error_mens_apparel
+      error_womens_apparel
+    end
+  end
+
+  def error_mens_apparel
+    errors.add(:mens_apparel, "Error: Men's Apparel is missing")
+  end
+
+  def error_womens_apparel
+    errors.add(:mens_apparel, "Error: Women's Apparel is missing")
+  end
+
+  private
+
+def check_employees
+    if self.employees.size > 0
+      error_destroy
+      throw :abort
+    end
+  end
+
+  def error_destroy
+    errors.add(:base, "Cannot destroy store with #{self.employees.size} employees.")
+  end
+
+end
+
 end
